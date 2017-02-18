6f4f16ef5d24b75b8f249052cb3bc309fc2b797b
Grzegorz Karlik <grzegorz.karlik@gmail.com>
2017-02-18 21:05:51 +0100

    Changing expense service default port.

--- expense-service/v1/run.sh
+++ expense-service/v1/run.sh
@@ -1,6 +1,6 @@
 export EXPENSE_SERVICE_NAME="ExpenseService"
 export EXPENSE_SERVICE_VERSION="v1"
-export EXPENSE_SERVICE_PORT="8888"
+export EXPENSE_SERVICE_PORT="5555"
 export DISCOVERY="http://localhost:9999"
 export DATABASE_DIALECT="postgres"
 export DATABASE="host=localhost user=postgres dbname=expense_service sslmode=disable password="
