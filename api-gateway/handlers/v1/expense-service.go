ed519bdc0f6205133b9f9e0c686e2a2ee99bf876
Grzegorz Karlik <grzegorz.karlik@gmail.com>
2017-02-21 19:14:54 +0100

    Fix logging in handlers.

--- api-gateway/handlers/v1/expense-service.go
+++ api-gateway/handlers/v1/expense-service.go
@@ -7,7 +7,9 @@ import (
 	"github.com/gkarlik/expense-tracker/shared/errors"
 	"github.com/gkarlik/expense-tracker/shared/handler"
 	"github.com/gkarlik/quark-go"
+	"github.com/gkarlik/quark-go/logger"
 	sd "github.com/gkarlik/quark-go/service/discovery"
+	"github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -26,14 +28,19 @@ func GetExpenseServiceConn(s quark.Service) (*grpc.ClientConn, error) {
 
 func UpdateExpenseHandler(s quark.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var expense es.ExpenseRequest
+		reqID := uuid.NewV4()
+		s.Log().DebugWithFields(logger.Fields{"requestID": reqID, "request": r}, "Update expense request")
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing update expense handler")
 
+		var expense es.ExpenseRequest
 		if err := handler.ParseRequestData(r, &expense); err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot parse request data")
 			handler.ErrorResponse(w, errors.ErrInvalidRequestData, http.StatusInternalServerError)
 			return
 		}
 		conn, err := GetExpenseServiceConn(s)
 		if err != nil || conn == nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to ExpenseService")
 			handler.ErrorResponse(w, errors.ErrInternal, http.StatusInternalServerError)
 			return
 		}
@@ -42,23 +49,31 @@ func UpdateExpenseHandler(s quark.Service) http.HandlerFunc {
 		expenseService := es.NewExpenseServiceClient(conn)
 		e, err := expenseService.UpdateExpense(context.Background(), &expense)
 		if err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot update expense")
 			handler.ErrorResponse(w, errors.ErrCannotUpdateExpense, http.StatusInternalServerError)
 			return
 		}
 		handler.Response(w, e)
+
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing update expense handler")
 	}
 }
 
 func UpdateCategoryHandler(s quark.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var category es.CategoryRequest
+		reqID := uuid.NewV4()
+		s.Log().DebugWithFields(logger.Fields{"requestID": reqID, "request": r}, "Update category request")
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing update category handler")
 
+		var category es.CategoryRequest
 		if err := handler.ParseRequestData(r, &category); err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot parse request data")
 			handler.ErrorResponse(w, errors.ErrInvalidRequestData, http.StatusInternalServerError)
 			return
 		}
 		conn, err := GetExpenseServiceConn(s)
 		if err != nil || conn == nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to ExpenseService")
 			handler.ErrorResponse(w, errors.ErrInternal, http.StatusInternalServerError)
 			return
 		}
@@ -67,9 +82,12 @@ func UpdateCategoryHandler(s quark.Service) http.HandlerFunc {
 		expenseService := es.NewExpenseServiceClient(conn)
 		c, err := expenseService.UpdateCategory(context.Background(), &category)
 		if err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot update category")
 			handler.ErrorResponse(w, errors.ErrCannotUpdateCategory, http.StatusInternalServerError)
 			return
 		}
 		handler.Response(w, c)
+
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing update category handler")
 	}
 }
--- api-gateway/handlers/v1/user-service.go
+++ api-gateway/handlers/v1/user-service.go
@@ -14,6 +14,7 @@ import (
 	"github.com/gkarlik/quark-go/logger"
 	sd "github.com/gkarlik/quark-go/service/discovery"
 	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -21,12 +22,10 @@ import (
 func GetUserServiceConn(s quark.Service) (*grpc.ClientConn, error) {
 	url, err := s.Discovery().GetServiceAddress(sd.ByName("UserService"), sd.ByVersion("v1"))
 	if err != nil || url == nil {
-		s.Log().ErrorWithFields(logger.Fields{"error": err}, "Cannot locate UserService")
 		return nil, err
 	}
 	conn, err := grpc.Dial(url.String(), grpc.WithInsecure())
 	if err != nil {
-		s.Log().ErrorWithFields(logger.Fields{"error": err, "address": url}, "Cannot dial address provided for UserService")
 		return nil, err
 	}
 	return conn, err
@@ -34,14 +33,20 @@ func GetUserServiceConn(s quark.Service) (*grpc.ClientConn, error) {
 
 func RegisterUserHandler(s quark.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var user us.UserRequest
+		reqID := uuid.NewV4()
+		s.Log().DebugWithFields(logger.Fields{"requestID": reqID, "request": r}, "Register user request")
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing register user handler")
 
+		var user us.UserRequest
 		if err := handler.ParseRequestData(r, &user); err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot parse request data")
 			handler.ErrorResponse(w, errors.ErrInvalidRequestData, http.StatusInternalServerError)
 			return
 		}
+
 		conn, err := GetUserServiceConn(s)
 		if err != nil || conn == nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to UserService")
 			handler.ErrorResponse(w, errors.ErrInternal, http.StatusInternalServerError)
 			return
 		}
@@ -50,6 +55,7 @@ func RegisterUserHandler(s quark.Service) http.HandlerFunc {
 		userService := us.NewUserServiceClient(conn)
 		_, err = userService.RegisterUser(context.Background(), &user)
 		if err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot register user")
 			if errors.ErrUserAlreadyExists.IsSame(err) {
 				handler.ErrorResponse(w, errors.ErrUserAlreadyExists, http.StatusConflict)
 				return
@@ -58,12 +64,18 @@ func RegisterUserHandler(s quark.Service) http.HandlerFunc {
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
+
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing register user handler")
 	}
 }
 
 func AuthenticateUser(s quark.Service, credentials auth.Credentials) (auth.Claims, error) {
+	reqID := uuid.NewV4()
+	s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing authenticate user handler")
+
 	conn, err := GetUserServiceConn(s)
 	if err != nil || conn == nil {
+		s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to UserService")
 		return auth.Claims{}, errors.ErrInternal
 	}
 	defer conn.Close()
@@ -75,8 +87,11 @@ func AuthenticateUser(s quark.Service, credentials auth.Credentials) (auth.Claim
 		Pin:      credentials.Properties["Pin"],
 	})
 	if err != nil {
+		s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot authenticate user")
 		return auth.Claims{}, errors.ErrInvalidUsernamePassword
 	}
+	s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing authenticate user handler")
+
 	return auth.Claims{
 		Username: credentials.Username,
 		StandardClaims: jwt.StandardClaims{
@@ -88,16 +103,22 @@ func AuthenticateUser(s quark.Service, credentials auth.Credentials) (auth.Claim
 
 func GetUserByLoginHandler(s quark.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		reqID := uuid.NewV4()
+		s.Log().DebugWithFields(logger.Fields{"requestID": reqID, "request": r}, "Get user by login request")
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing get user by login handler")
+
 		q := r.URL.Query()
 		login := q.Get("login")
 
 		if login == "" {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID}, "Missing 'login' parameter in request")
 			handler.ErrorResponse(w, errors.ErrInvalidRequestParameters, http.StatusBadRequest)
 			return
 		}
 
 		conn, err := GetUserServiceConn(s)
 		if err != nil || conn == nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to UserService")
 			handler.ErrorResponse(w, errors.ErrInternal, http.StatusInternalServerError)
 			return
 		}
@@ -106,25 +127,34 @@ func GetUserByLoginHandler(s quark.Service) http.HandlerFunc {
 		userService := us.NewUserServiceClient(conn)
 		user, err := userService.GetUserByLogin(context.Background(), &us.UserLoginRequest{Login: login})
 		if err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot get user by login")
 			handler.ErrorResponse(w, errors.ErrUserNotFound, http.StatusNotFound)
 			return
 		}
 		handler.Response(w, user)
+
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing get user by login handler")
 	}
 }
 
 func GetUserByIDHandler(s quark.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		reqID := uuid.NewV4()
+		s.Log().DebugWithFields(logger.Fields{"requestID": reqID, "request": r}, "Get user by ID request")
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Processing get user by ID handler")
+
 		userID := mux.Vars(r)["id"]
 		id, err := strconv.Atoi(userID)
 
 		if userID == "" || err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Missing 'ID' parameter in request")
 			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
 		}
 
 		conn, err := GetUserServiceConn(s)
 		if err != nil || conn == nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot connect to UserService")
 			handler.ErrorResponse(w, errors.ErrInvalidRequestParameters, http.StatusBadRequest)
 			return
 		}
@@ -133,9 +163,12 @@ func GetUserByIDHandler(s quark.Service) http.HandlerFunc {
 		userService := us.NewUserServiceClient(conn)
 		user, err := userService.GetUserByID(context.Background(), &us.UserIDRequest{ID: uint32(id)})
 		if err != nil {
+			s.Log().ErrorWithFields(logger.Fields{"requestID": reqID, "error": err}, "Cannot get user by ID")
 			handler.ErrorResponse(w, errors.ErrUserNotFound, http.StatusNotFound)
 			return
 		}
 		handler.Response(w, user)
+
+		s.Log().InfoWithFields(logger.Fields{"requestID": reqID}, "Done processing get user by ID handler")
 	}
 }
